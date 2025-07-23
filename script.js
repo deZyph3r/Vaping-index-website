1025f97fc3b44914fce0df68dbff46a018235478
deZyph3r <epicgamerlolmm@gmail.com>
2025-07-23 14:26:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show animations when scrolling
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.2,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appear
