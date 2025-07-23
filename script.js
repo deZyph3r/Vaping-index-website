55b1b664a26bd33c30128e3eefb01416ae2056c8
deZyph3r <epicgamerlolmm@gmail.com>
2025-07-23 14:41:00 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Show animations when scrolling
-const faders = document.querySelectorAll('.fade-in');
-
-const appearOptions = {
-  threshold: 0.2,
-  rootMargin: "0px 0px -50px 0px"
-};
-
-const appear
